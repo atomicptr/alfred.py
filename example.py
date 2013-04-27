c77725dd46280b25b8d1363aa90679f5f6d50c71
Christopher Kaster <ikasoki@gmail.com>
2013-04-27 13:20:57 +0200

    add more explanations to example.py

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 import sys
 import Alfred
 
+""" IMPORTANT: Check out the "Alfred.py" file for more information about the methods! """
+
 # create a new alfred handler. It's important to add either a query or the system arguments
 # example with an example query:
 #handler = Alfred.Handler(query="QUERY")
@@ -8,8 +10,12 @@ import Alfred
 # example with system arguments
 handler = Alfred.Handler(args=sys.argv)
 
+# Add items with add_item
 handler.add_item(Alfred.Item(title="Example entry #1", subtitle="best entry!", icon="cake.png", uid=1, arg="#1"))
 handler.add_item(Alfred.Item(title="Entry #2", subtitle="Cake!", uid=815, arg="#2"))
+
+# Add item with add_new_item
 handler.add_new_item(title="Sir, you're doing it wrong!", subtitle=handler.query, arg="Hello", uid="cake")
 
+# push the items to Alfred
 handler.push(max_results=3)
